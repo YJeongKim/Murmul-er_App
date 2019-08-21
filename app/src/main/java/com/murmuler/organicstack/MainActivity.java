52700bcd7c9681b2743a16b90d58d7d53a57bbae
ine <enchanted13th@gmail.com>
2019-08-21 16:39:13 +0900

    [ISSUE-1] 검색 화면 구현 - 네이버 지도 API 연동 - 전체적인 틀 레이아웃 잡음

--- app/src/main/java/com/murmuler/organicstack/MainActivity.java
+++ app/src/main/java/com/murmuler/organicstack/MainActivity.java
@@ -2,13 +2,29 @@ package com.murmuler.organicstack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editSearch;
+    ImageButton btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editSearch = findViewById(R.id.editText);
+        btnSearch = findViewById(R.id.btnSearch);
+    }
+
+    public void onClick(View view) {
+        String keyword = editSearch.getText().toString();
+        Toast.makeText(view.getContext(), keyword, Toast.LENGTH_LONG).show();
     }
 }
