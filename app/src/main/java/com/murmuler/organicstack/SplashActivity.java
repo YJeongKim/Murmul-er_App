f0818539b67e23acaac0f913fb8de902e5c68b5c
seokju2ng <ehddnwnd@naver.com>
2019-08-22 22:38:18 +0900

    [ISSUE-7] 통합...(1)
    - 검색은 아직 안붙임...

--- app/src/main/java/com/murmuler/organicstack/SplashActivity.java
+++ app/src/main/java/com/murmuler/organicstack/SplashActivity.java
@@ -1,34 +1,50 @@
 package com.murmuler.organicstack;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
-public class SplashActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    private final Handler handler = new Handler();
+//    RequestManager glideRequestManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
 
-//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 //        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
 //        Glide.with(this).load(R.raw.house).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(imageViewTarget);
 
+        Glide.with(this).asGif().load(R.raw.house).into(imageView);
 //        startLoading();
-    }
 
-    private void startLoading() {
-        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+
+    private void startLoading() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 finish();
             }
         }, 3000);
